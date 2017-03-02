b28ab265c682c9aa38923958095f094b575560e2
José Estela <jose.estela@gigigo.com>
2017-03-02 19:44:18 +0100

    Initial Commit

---
+++ Move/Move.h
@@ -0,0 +1,19 @@
+//
+//  Move.h
+//  Move
+//
+//  Created by José Estela on 2/3/17.
+//  Copyright © 2017 José Estela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Move.
+FOUNDATION_EXPORT double MoveVersionNumber;
+
+//! Project version string for Move.
+FOUNDATION_EXPORT const unsigned char MoveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Move/PublicHeader.h>
+
+
